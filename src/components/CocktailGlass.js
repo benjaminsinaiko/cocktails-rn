ec0a74854735acefaa346c399110e6a115799fe9
benjaminsinaiko <ben.jamin@outlook.com>
2020-04-30 15:49:54 -0500

    Add cocktail glass icon to details

---
+++ src/components/CocktailGlass.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Avatar, useTheme } from '@ui-kitten/components';
+
+const CocktailGlass = ({ glass }) => {
+  const theme = useTheme();
+  return (
+    <View style={styles.container}>
+      <Avatar
+        style={{ tintColor: theme['color-warning-default'] }}
+        source={require(`../icons/${glass}.png`)}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingVertical: 10,
+    width: 75,
+  },
+});
+
+export default CocktailGlass;
